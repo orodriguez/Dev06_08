71e703e6a78b4e1ee360a898df2b181e686bd6cb
“orodriguez” <omarjavier15@gmail.com>
2024-03-06 11:54:36 -0400

    Playing with DI

---
+++ Okane.Tests/DependencyInjectionTests.cs
@@ -0,0 +1,72 @@
+using Microsoft.Extensions.DependencyInjection;
+using Okane.Application;
+
+namespace Okane.Tests;
+
+public class DependencyInjectionTests
+{
+    [Fact]
+    public void ManualInjection()
+    {
+        var service = new ExpenseService(new InMemoryExpensesRepository());
+    }
+
+    [Fact]
+    public void DependencyInjectionContainer()
+    {
+        var services = new ServiceCollection();
+        services.AddTransient<IExpenseService, ExpenseService>();
+        services.AddSingleton<IExpensesRepository, InMemoryExpensesRepository>();
+
+        var provider = services.BuildServiceProvider();
+
+        var expensesService = provider.GetRequiredService<IExpenseService>();
+        
+        Assert.Empty(expensesService.RetrieveAll());
+    }
+    
+    [Fact]
+    public void UnableToResolve()
+    {
+        var services = new ServiceCollection();
+        services.AddTransient<IExpenseService, ExpenseService>();
+        
+
+        var provider = services.BuildServiceProvider();
+
+        var exception = Assert.Throws<InvalidOperationException>(() => 
+            provider.GetRequiredService<IExpenseService>());
+
+        Assert.Contains("Unable to resolve service", 
+            exception.Message);
+    }
+
+    [Fact]
+    public void Transient()
+    {
+        var services = new ServiceCollection();
+        services.AddTransient<IExpenseService, ExpenseService>();
+        services.AddTransient<IExpensesRepository, InMemoryExpensesRepository>();
+        var provider = services.BuildServiceProvider();
+
+        var expenseService1 = provider.GetRequiredService<IExpenseService>();
+        var expenseService2 = provider.GetRequiredService<IExpenseService>();
+
+        Assert.NotEqual(expenseService1.GetHashCode(),
+            expenseService2.GetHashCode());
+    }
+    
+    [Fact]
+    public void Singleton()
+    {
+        var services = new ServiceCollection();
+        services.AddSingleton<IExpensesRepository, InMemoryExpensesRepository>();
+        var provider = services.BuildServiceProvider();
+
+        var repository1 = provider.GetRequiredService<IExpensesRepository>();
+        var repository2 = provider.GetRequiredService<IExpensesRepository>();
+
+        Assert.Equal(repository1.GetHashCode(),
+            repository2.GetHashCode());
+    }
+}
