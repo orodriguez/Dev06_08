de1a197fca9b9bb30ba2fab58b1c8c9efef37d9b
Jose Enrique Morel Martinez <morelmartinezj@gmail.com>
2024-03-06 23:21:40 -0400

    jose enrique morel martinez

--- Okane.Application/ExpenseService.cs
+++ Okane.Application/ExpenseService.cs
@@ -17,4 +17,60 @@ public class ExpenseService : IExpenseService
 
     public IEnumerable<Expense> RetrieveAll() => 
         _expenses.All();
+
+    public void DeleteExpense(int id)
+    {
+        _expenses.Delete(id);
+    }
+}
+
+[ApiController]
+[Route("api/[controller]")]
+public class ExpensesController : ControllerBase
+{
+    private readonly IExpenseService _expenseService;
+
+    public ExpensesController(IExpenseService expenseService) =>
+        _expenseService = expenseService;
+
+    [HttpPost]
+    public IActionResult Post([FromBody] ExpenseDto expenseDto)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+        var expense = expenseDto.ToExpense();
+        _expenseService.RegisterExpense(expense);
+
+        return CreatedAtAction(nameof(Get), new { id = expense.Id }, expense);
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        var expenses = _expenseService.RetrieveAll();
+        return Ok(expenses);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult Get(int id)
+    {
+        var expense = _expenseService.RetrieveById(id);
+        if (expense == null)
+            return NotFound();
+
+        return Ok(expense);
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        var expense = _expenseService.RetrieveById(id);
+        if (expense == null)
+            return NotFound();
+
+        _expenseService.DeleteExpense(id);
+
+        return NoContent();
+    }
 }
