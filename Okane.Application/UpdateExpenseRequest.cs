6e49d69d9deaf64edd09ec3d81796356218c6bbf
Ismael Varela <info@varelait.com>
2024-03-12 23:58:30 -0400

    validation for InvoiceUrl property

--- Okane.Application/UpdateExpenseRequest.cs
+++ Okane.Application/UpdateExpenseRequest.cs
@@ -1,8 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Okane.Application;
 
 public class UpdateExpenseRequest
 {
-    public required string Category { get; set; }
+    [Required]
+    [Range(1, 1_000_000, ErrorMessage = "Amount is out of range")]
     public int Amount { get; set; }
+    
+    [Required]
+    [MaxLength(80, ErrorMessage = "Category is too long")]
+    public required string Category { get; set; }
+    
+    [MaxLength(250, ErrorMessage = "Description is too long")]
     public string? Description { get; set; }
+
+    [RegularExpression(@"((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)", ErrorMessage = "Not a valid website URL")]
+    public string? InvoiceUrl { get; set; }
 }
