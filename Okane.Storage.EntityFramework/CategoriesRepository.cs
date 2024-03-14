c5fb8db1ddd028f3af3d922cab908466639d3954
Ismael Varela <info@varelait.com>
2024-03-14 16:58:48 -0400

    create category if not found

--- Okane.Storage.EntityFramework/CategoriesRepository.cs
+++ Okane.Storage.EntityFramework/CategoriesRepository.cs
@@ -1,3 +1,4 @@
+using System.Reflection.Metadata.Ecma335;
 using Okane.Application;
 using Okane.Domain;
 using Okane.Storage.EntityFramework;
@@ -8,6 +9,24 @@ public class CategoriesRepository : ICategoriesRepository
 
     public CategoriesRepository(OkaneDbContext db) => _db = db;
 
-    public Category ByName(string categoryName) => 
-        _db.Categories.First(category => category.Name == categoryName);
+    public Category ByName(string categoryName)
+    {
+        var result = _db.Categories.FirstOrDefault(category => category.Name == categoryName);
+
+        if (result != null)
+        {
+            return result;
+        }
+
+        Category newCategory = new Category
+        {
+            Name = categoryName
+        };
+
+        _db.Categories.Add(newCategory);
+        _db.SaveChanges();
+        result = _db.Categories.First(category => category.Name == categoryName);
+
+        return result;
+    }
 }
