3c8ae08e593f90305d6b46cc597d65f4f8e33a1f
Emily De La Rosa <emilybiandra@icloud.com>
2024-03-22 00:57:33 -0400

    cambios tarea 3

--- Okane.Application/CreateExpenseRequest.cs
+++ Okane.Application/CreateExpenseRequest.cs
@@ -1,20 +1,21 @@
 using System.ComponentModel.DataAnnotations;
 
-namespace Okane.Application;
-
-public class CreateExpenseRequest
+namespace Okane.Application
 {
-    [Required]
-    [Range(1, 1_000_000, ErrorMessage = "Amount is out of range")]
-    public int Amount { get; set; }
-    
-    [Required]
-    [MaxLength(80, ErrorMessage = "Category is too long")]
-    public required string CategoryName { get; set; }
-    
-    [MaxLength(250, ErrorMessage = "Description is too long")]
-    public string? Description { get; set; }
-    
-    [Url]
-    public string? InvoiceUrl { get; set; }
+    public class CreateExpenseRequest
+    {
+        [Required]
+        [Range(1, 1_000_000, ErrorMessage = "Amount is out of range")]
+        public int Amount { get; set; }
+        
+        [Required]
+        [MaxLength(80, ErrorMessage = "Category is too long")]
+        public required string CategoryName { get; set; }
+        
+        [MaxLength(250, ErrorMessage = "Description is too long")]
+        public string? Description { get; set; }
+        
+        [Url(ErrorMessage = "Invalid URL")]
+        public string? InvoiceUrl { get; set; }
+    }
 }
--- Okane.Domain/Expense.cs
+++ Okane.Domain/Expense.cs
@@ -1,16 +1,19 @@
-namespace Okane.Domain;
+using System;
 
-public class Expense
+namespace Okane.Domain
 {
-    public int Id { get; set; }
-    public int Amount { get; set; }
-    public string CategoryName => Category.Name;
-    public string? Description { get; set; }
-    public string? InvoiceUrl { get; set; }
-    public DateTime CreatedAt { get; set; }
-    public DateTime UpdatedAt { get; set; }
-    
-    public int CategoryId { get; set; }
-    public required Category Category { get; set; }
-    public required User User { get; set; }
+    public class Expense
+    {
+        public int Id { get; set; }
+        public int Amount { get; set; }
+        public string CategoryName => Category.Name;
+        public string? Description { get; set; }
+        public string? InvoiceUrl { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; } // Nueva propiedad para almacenar la fecha de actualización
+
+        public int CategoryId { get; set; }
+        public required Category Category { get; set; }
+        public required User User { get; set; }
+    }
 }
